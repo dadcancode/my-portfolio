2188e62560bfecda40c9f1fddeca30d74ebeb0e1
Zach <zachery.goodrich@gmail.com>
2020-07-18 16:50:05 -0700

    added build

---
+++ portfolio-frontend/build/precache-manifest.890c6fc85d26ed7328b49c662b3449ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbfc0301120e0dbbf6f90164dc0435ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4d76ea862f935a51341",
+    "url": "/static/css/main.358ff34c.chunk.css"
+  },
+  {
+    "revision": "03654a3d703c8c7369dd",
+    "url": "/static/js/2.bfb31e88.chunk.js"
+  },
+  {
+    "revision": "7a29fd67914f99c562dc3f65bbf1078b",
+    "url": "/static/js/2.bfb31e88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d76ea862f935a51341",
+    "url": "/static/js/main.ff39a0a9.chunk.js"
+  },
+  {
+    "revision": "afbaf1b86eb5ed055c4b",
+    "url": "/static/js/runtime-main.3717989d.js"
+  },
+  {
+    "revision": "e422bea17671809d424853d7ce127daf",
+    "url": "/static/media/systematic_thinking.e422bea1.png"
+  },
+  {
+    "revision": "40d5f9f4d31f2eff40f926a0875308e2",
+    "url": "/static/media/tic-tac-attack-screenshot.40d5f9f4.PNG"
+  }
+]);
