1682a10dc013b944972c066e93bba73af415af57
Zach <zachery.goodrich@gmail.com>
2020-08-12 19:51:38 -0700

    added some styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 
 function Navigation(props) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg sticky-top navbar-light bg-light justify-content-between">
             <a class="navbar-brand" href="#">ZACHERY GOODRICH</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
                         <a class="nav-link" href="/">About</a>
                     </li>
                     <li class="nav-item">
